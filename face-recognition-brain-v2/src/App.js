fef8d8333dc2bf3d88ee2cdd80bea3b6c6135c8e
joeban0608 <80736596+joeban0608@users.noreply.github.com>
2021-10-17 22:10:44 +0800

    push server and front-end folder

---
+++ face-recognition-brain-v2/src/App.js
@@ -0,0 +1,176 @@
+import './App.css';
+import React,{Component} from 'react';
+import Navigation from './component/Navigation/Navigation';
+import Logo from './component/Logo/Logo'
+import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm'
+import Rank from './component/Rank/Rank'
+import FaceRecognition from './component/FaceRecognition/FaceRecognition'
+import Register from './component/Register/Register';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
+import Signin from './component/Signin/Signin'
+
+const app = new Clarifai.App({
+//  apiKey: 'YOUR_API_KEY'
+ apiKey: 'b8962c2705744018a0bd25cbc1434e74'
+});
+
+// particles setting
+const ParticlesOptions = {
+  particles: {
+    number : {
+      value :80,
+      density : {
+        enable: true,
+        value_area: 800
+      }
+    },
+    size: {
+        value: 3
+    },
+    opacity: {
+      value: 1
+    },
+    move: {
+      speed: 3
+    },
+  },
+  interactivity:{
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box:{},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        // password: 'cookies',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
+  // the callback fucntion in onButtonSubit method on setState function in order to calculate the data from Clarifai api
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = image.width;
+    const height = image.height;
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: (1 - clarifaiFace.right_col) * width ,
+      bottomRow: (1 - clarifaiFace.bottom_row) * height
+    }
+  }
+
+  //the callback fucntion in onButtonSubmit to set the state for box in order to displayFaceBox
+  setStateBox = (box) => {
+    // console.log(box)
+    this.setState({box:box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input:event.target.value});
+  }
+
+  //submit picture to detect the face
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+        // .then(response => this.setStateBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:2999/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            // this.setState({user:{
+            //   entries: count.entries
+            //   }
+            // })
+            this.setState(Object.assign(this.state.user, {entries: count.entries}))
+            // console.log('userInfo:', this.state.user)
+          })
+        this.setStateBox(this.calculateFaceLocation(response))
+      }})
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn:false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route: route})
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+        <div className="App">
+          <Particles className="particles" params={ParticlesOptions}/>
+          {/* <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/> */}
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+          { route === 'home' 
+            ? <div>
+                <Logo/>
+                <Rank 
+                  name={this.state.user.name}
+                  entries={this.state.user.entries}
+                />
+                <ImageLinkForm 
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+                />
+                {/* <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> */}
+                <FaceRecognition box={box} imageUrl={imageUrl} />
+              </div>
+            : (
+                route === 'signin'
+                ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              )
+          }
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ smart-brain-api/server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bird',
+            entries: 0,
+            joined: new Date()
+        },
+    ],
+    // login: [
+    //     {
+    //         id: '987',
+    //         hash: '',
+    //         email: 'john@gmail.com'
+    //     }
+    // ]
+}
+
+app.get('/', (req, res)=> {
+    // res.send('this is working')
+    res.send(database.users)
+})
+
+app.post('/signin', (req,res) => {
+    if (req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password) {
+        // res.json('success')
+        res.json(database.users[0])
+    } else {
+        res.status(400).json('error logging in')
+    }
+})
+
+app.post('/register', (req,res) => {
+    const { email, name, password } = req.body;
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     console.log(hash)
+    // });
+    
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        // password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]) // 取最後一個加入database的user
+})
+
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user)
+        }
+    })
+    if (!found) {
+        res.status(404).json('not found')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries ++;
+            return res.json({entries:user.entries})
+        }
+    })
+    if (!found) {
+        res.status(404).json('not found')
+    }
+})
+
+app.listen(2999, () => {
+    console.log('app is running on port 2999')
+})
+
+
